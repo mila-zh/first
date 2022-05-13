392e100c2d0e140e222fe25000fafb6f2b76bf92
Mila Zhurba <mila.zh.7@gmail.com>
2022-05-13 10:15:29 -0400

    my first commit Mila Zh

---
+++ week1.js
@@ -0,0 +1,31 @@
+//spread, rest
+
+var myValue = Math.max(1,2,3,4,5,6,7,8);
+//console.log(myValue);
+
+function sum(...args){ //rest operator
+let numSum = 0;
+for(const arg of args){
+
+numSum+=arg;
+}
+return numSum;
+}
+
+var myArray =[4,5,1,2];
+console.log(sum(...myArray));//spread operation
+
+
+//Design a function to return multiplication of first two arguments, and sum of the rest
+
+function sumX(a,b,...args){
+    let sum;
+    let restSum;
+    sum = a + b;
+    for (const arg of args){
+        restSum += arg;
+    }
+    return (sum, restSum);
+}
+
+sumX(2, 2,3)
